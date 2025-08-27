53db2f1f4c0e008f18a8cda4ab4d10df846596c3
Salman Tariq <salmantariqofficial@gmail.com>
2025-08-27 10:21:32 +0500

    aos added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
+import { useEffect } from "react";
 import './App.css'
 import './Theme.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function App() {
+
+    useEffect(() => {
+    AOS.init({
+      duration: 1000, // animation duration
+      once: true,     // whether animation should happen only once
+    });
+  }, []);
+
   return (
     <>
-      <Header />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-      <Footer />
+    <div className="main overflow-x-hidden">
+        <Header />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+        <Footer />
+    </div>
     </>
   )
 }
