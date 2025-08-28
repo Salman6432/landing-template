43c19779390a7e2b453e8f2dd182a2a05a737645
Muhammad Bilal <bs131400@gmail.com>
2025-08-28 23:13:42 +0500

    ui changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,13 +21,14 @@ const Home = () => {
       </section>
 
       {/* About Us */}
-      <section className='py-[100px]'>
+      <section className='py-[100px] bg-white'>
         <div className="custom-container">
             <div className="grid grid-cols-2 gap-10 items-center">
                 <div>
                     <img src="/assets/imgs/hero-img.png" className='w-full h-full max-w-[500px] mr-auto' alt="" />
                 </div>
                 <div>
+                    <div className="section-title-bage"><span>About Us</span></div>
                     <h2 className='mb-4'>About Us</h2>
                     <p className='mb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste minus quibusdam ipsa illo quisquam quo magni minima libero doloremque dolores.</p>
                     <Button variant='default' className="p-6 text-md relative z-0 overflow-hidden">Get Started</Button>
@@ -39,14 +40,83 @@ const Home = () => {
       {/* Cards */}
       <section className='my-[100px]'>
         <div className="custom-container">
+          <div className='flex flex-col justify-center items-center text-center mb-10'>
+            <div className="section-title-bage"><span>Services</span></div>
+            <h2>Lorem ipsum dolor sit amet consectetur</h2>
+          </div>
             <div className="grid grid-cols-3 gap-4 items-center">
-                <div className="card border border-gray-200 p-6 flex flex-col gap-4 bg-white rounded-xl">
-                    <BadgeCheck size={60} className='mb-[40px]'/>
-                    <h4>hello world</h4>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum, voluptatibus.</p>
-                    <a href="" className='flex gap-2 items-center'>Check Now <ArrowUpRight/></a>
+                <div className="gradiant-hover-card">
+                  <div className="card p-6 flex flex-col gap-4">
+                      <div className="icon text-primary">
+                        <BadgeCheck size={60} className='mb-[40px]'/>
+                      </div>
+                      <h4 className='text-primary'>hello world</h4>
+                      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum, voluptatibus.</p>
+                      <a href="" className='flex gap-2 items-center text-primary'>Check Now <ArrowUpRight/></a>
+                  </div>
                 </div>
-                
+                <div className="gradiant-hover-card">
+                  <div className="card p-6 flex flex-col gap-4">
+                      <div className="icon text-primary">
+                        <BadgeCheck size={60} className='mb-[40px]'/>
+                      </div>
+                      <h4 className='text-primary'>hello world</h4>
+                      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum, voluptatibus.</p>
+                      <a href="" className='flex gap-2 items-center text-primary'>Check Now <ArrowUpRight/></a>
+                  </div>
+                </div>
+                <div className="gradiant-hover-card">
+                  <div className="card p-6 flex flex-col gap-4">
+                      <div className="icon text-primary">
+                        <BadgeCheck size={60} className='mb-[40px]'/>
+                      </div>
+                      <h4 className='text-primary'>hello world</h4>
+                      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum, voluptatibus.</p>
+                      <a href="" className='flex gap-2 items-center text-primary'>Check Now <ArrowUpRight/></a>
+                  </div>
+                </div>
+            </div>
+        </div>
+      </section>
+
+      {/* pricing */}
+      <section className='py-[100px] bg-white'>
+        <div className="custom-container">
+          <div className='flex flex-col justify-center items-center text-center mb-10'>
+            <div className="section-title-bage"><span>Pricing</span></div>
+            <h2>Lorem ipsum dolor sit amet consectetur</h2>
+          </div>
+            <div className="grid grid-cols-3 gap-4 items-center">
+                <div className="gradiant-hover-card">
+                  <div className="card p-6 flex flex-col gap-4">
+                      <div className="icon text-primary">
+                        <BadgeCheck size={60} className='mb-[40px]'/>
+                      </div>
+                      <h4 className='text-primary'>hello world</h4>
+                      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum, voluptatibus.</p>
+                      <a href="" className='flex gap-2 items-center text-primary'>Check Now <ArrowUpRight/></a>
+                  </div>
+                </div>
+                <div className="gradiant-hover-card">
+                  <div className="card p-6 flex flex-col gap-4">
+                      <div className="icon text-primary">
+                        <BadgeCheck size={60} className='mb-[40px]'/>
+                      </div>
+                      <h4 className='text-primary'>hello world</h4>
+                      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum, voluptatibus.</p>
+                      <a href="" className='flex gap-2 items-center text-primary'>Check Now <ArrowUpRight/></a>
+                  </div>
+                </div>
+                <div className="gradiant-hover-card">
+                  <div className="card p-6 flex flex-col gap-4">
+                      <div className="icon text-primary">
+                        <BadgeCheck size={60} className='mb-[40px]'/>
+                      </div>
+                      <h4 className='text-primary'>hello world</h4>
+                      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum, voluptatibus.</p>
+                      <a href="" className='flex gap-2 items-center text-primary'>Check Now <ArrowUpRight/></a>
+                  </div>
+                </div>
             </div>
         </div>
       </section>
